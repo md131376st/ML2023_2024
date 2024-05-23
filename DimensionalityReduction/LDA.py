d99f94532591b64d9eba23544eebc5d524cbdbc1
davarimona <m.davari@reply.it>
2024-05-23 16:46:08 +0200

    last changes

--- DimensionalityReduction/LDA.py
+++ DimensionalityReduction/LDA.py
@@ -1,89 +1,137 @@
-import sklearn.datasets
 import numpy as np
-import pylab
-import scipy
+import matplotlib.pyplot as plt
+import scipy.linalg
 
 
 class LDA:
-    def __init__(self, m):
+    def __init__(self, m, test_size=0.2, random_state=None):
         self.m = m
-        self.data = np.ndarray
-        self.labels = np.ndarray
-        self.SampleSizeDiffClass = np.ndarray
-        self.eachClassMean = np.ndarray
-        self.sb = 0
-        self.sw = 0
+        self.test_size = test_size
+        self.random_state = random_state
+
+        self.data = None
+        self.labels = None
+        self.train_data = None
+        self.train_labels = None
+        self.val_data = None
+        self.val_labels = None
+
+        self.SampleSizeDiffClass = None
+        self.eachClassMean = None
+        self.sb = None
+        self.sw = None
+
         self.LoadData()
+        self.SplitData()
         self.NumberOfClassSample()
-        self.SumAllDataValues = sum(self.SampleSizeDiffClass)
-        self.SampleMean = self.CalculateMean(self.data)
+        self.SumAllDataValues = np.sum(self.SampleSizeDiffClass)
+        self.SampleMean = self.CalculateMean(self.train_data)
         self.CalculateClassMean()
         self.CalculateSb()
         self.CalculateSW()
-        self.CalculeteEginValueJoinSB()
-        # self.CalculeteEginValue()
-        self.PlotFunction()
+        self.CalculateEigenValueJoinSb()
+        self.FixOrientation()
+        self.Threshold = self.CalculateThreshold()
+        self.PlotHistograms()
+        self.error_rate = self.ComputeErrorRate()
+        print(f"Validation Error Rate: {self.error_rate}")
 
-        pass
+    def SplitData(self):
+        if self.random_state is not None:
+            np.random.seed(self.random_state)
+        indices = np.arange(self.data.shape[1])
+        np.random.shuffle(indices)
 
-    def CalculeteEginValue(self):
-        # Generalized eigen value problem
+        split_point = int((1 - self.test_size) * len(indices))
+        train_indices = indices[:split_point]
+        val_indices = indices[split_point:]
+
+        self.train_data = self.data[:, train_indices]
+        self.train_labels = self.labels[train_indices]
+        self.val_data = self.data[:, val_indices]
+        self.val_labels = self.labels[val_indices]
+
+    def CalculateEigenValue(self):
+        # Generalized eigenvalue problem
         s, U = scipy.linalg.eigh(self.sb, self.sw)
-        W = U[:, ::-1][:, 0:self.m]
-        # find base value
+        W = U[:, ::-1][:, :self.m]
+        # Find base value
         UW, _, _ = np.linalg.svd(W)
-        self.eginVector = UW[:, 0:self.m]
+        self.eigenVector = UW[:, :self.m]
 
-    def CalculeteEginValueJoinSB(self):
-        # Generalized eigen value problem
+    def CalculateEigenValueJoinSb(self):
+        # Generalized eigenvalue problem
         U, s, _ = np.linalg.svd(self.sw)
-        p1 = np.dot(np.dot(U, np.diag(1.0 / (s ** 0.5))), U.T)
+        p1 = np.dot(np.dot(U, np.diag(1.0 / np.sqrt(s))), U.T)
         sbt = np.dot(np.dot(p1, self.sb), p1.T)
         s, P2 = np.linalg.eigh(sbt)
-        P2= P2[:, ::-1][:, 0:self.m]
+        P2 = P2[:, ::-1][:, :self.m]
 
-        self.eginVector = np.dot(p1.T,P2 )
+        self.eigenVector = np.dot(p1.T, P2)
 
-    def PlotFunction(self):
-        projection_list = np.dot(self.eginVector.T, self.data)
-        men = projection_list[:, self.labels == 0]
-        women = projection_list[:, self.labels == 1]
+    def FixOrientation(self):
+        projection_list = np.dot(self.eigenVector.T, self.train_data)
+        mean_false = projection_list[:, self.train_labels == 0].mean()
+        mean_true = projection_list[:, self.train_labels == 1].mean()
+        if mean_false > mean_true:
+            self.eigenVector = -self.eigenVector
 
-        pylab.scatter(men[0, :], men[1, :])
-        pylab.scatter(women[0, :], women[1, :])
-        pylab.xlabel('Principal component 1')
-        pylab.ylabel('Principal component 2')
-        pylab.legend(['men', 'women', "allData"])
-        pylab.show()
+    def CalculateThreshold(self):
+        projection_list = np.dot(self.eigenVector.T, self.train_data)
+        mean_false = projection_list[:, self.train_labels == 0].mean()
+        mean_true = projection_list[:, self.train_labels == 1].mean()
+        return (mean_false + mean_true) / 2
+
+    def ComputeErrorRate(self):
+        val_projection = np.dot(self.eigenVector.T, self.val_data)
+        predictions = val_projection > self.Threshold
+        error_rate = np.mean(predictions != self.val_labels)
+        return error_rate
+
+    def PlotHistograms(self):
+        projection_list = np.dot(self.eigenVector.T, self.train_data)
+        fake = projection_list[self.train_labels == 0]
+        genuine = projection_list[self.train_labels == 1]
+
+        plt.hist(fake, bins=20, alpha=0.4, label='fake')
+        plt.hist(genuine, bins=20, alpha=0.4, label='genuine')
+        # plt.axvline(x=self.Threshold, color='r', linestyle='--', label='Threshold')
+        plt.xlabel('Projected LDA value')
+        plt.ylabel('Frequency')
+        plt.legend(loc='upper right')
+        plt.title('Histogram of LDA projected samples')
+        plt.show()
 
     def NumberOfClassSample(self):
-        self.SampleSizeDiffClass = np.array([np.sum(self.labels == i) for i in set(self.labels)])
+        self.SampleSizeDiffClass = np.array([np.sum(self.train_labels == i) for i in np.unique(self.train_labels)])
 
     def CalculateClassMean(self):
-        self.eachClassMean = np.array([self.CalculateMean(self.data[:, self.labels == i]) for i in set(self.labels)]).T
+        self.eachClassMean = np.array(
+            [self.CalculateMean(self.train_data[:, self.train_labels == i]) for i in np.unique(self.train_labels)]).T
 
     def CalculateDiffMeanClassAndMeanDataset(self):
         return self.eachClassMean - self.VectorCol(self.SampleMean)
 
     def CalculateSb(self):
-        for i in range(self.SampleSizeDiffClass.size):
-            # mc-m
+        self.sb = np.zeros((self.train_data.shape[0], self.train_data.shape[0]))
+        for i in range(len(self.SampleSizeDiffClass)):
             diff_means = self.CalculateDiffMeanClassAndMeanDataset()[:, i:i + 1]
             self.sb += self.SampleSizeDiffClass[i] * np.dot(diff_means, diff_means.T)
         self.sb /= self.SumAllDataValues
 
     def CalculateSW(self):
-        for i in range(self.SampleSizeDiffClass.size):
-            classData = self.data[:, self.labels == i]
+        self.sw = np.zeros((self.train_data.shape[0], self.train_data.shape[0]))
+        for i in range(len(self.SampleSizeDiffClass)):
+            classData = self.train_data[:, self.train_labels == i]
             centerData = classData - self.VectorCol(self.eachClassMean.T[i])
             SWc = 1 / self.SampleSizeDiffClass[i] * np.dot(centerData, centerData.T)
             self.sw += self.SampleSizeDiffClass[i] * SWc
         self.sw /= self.SumAllDataValues
 
     def LoadData(self):
-        self.data = np.genfromtxt("../Data/Train.txt", delimiter=",")
-        self.labels = self.data[:, -1].T
-        self.data = self.data[:, :-1].T
+        data = np.genfromtxt("../Data/Train.txt", delimiter=",")
+        self.labels = data[:, -1].astype(int)
+        self.data = data[:, :-1].T
 
     def VectorCol(self, data):
         return data.reshape((data.size, 1))
@@ -92,7 +140,7 @@ class LDA:
         return data.reshape((1, data.size))
 
     def CalculateMean(self, data):
-        return data.mean(1)
+        return data.mean(axis=1)
 
 
-lda = LDA(2)
+lda = LDA(1, test_size=0.2, random_state=42)
--- DimensionalityReduction/PCA.py
+++ DimensionalityReduction/PCA.py
@@ -1,7 +1,7 @@
 import os
 
 import numpy as np
-from matplotlib import pylab
+import matplotlib.pyplot as plt
 from DimensionalityReduction.Utility import VectorCol
 
 
@@ -13,7 +13,7 @@ class PCA:
         self.egin_vector = []
         self.mean = 0
         self.conversion = 0
-        if data.any():
+        if data!=None:
             self.label = data[:, -1].T
             self.data = data[:, :-1].T
         else:
@@ -24,13 +24,12 @@ class PCA:
         self.CenterData()
         self.ConversionMatrix()
         self.Eigenvectors()
-        self.PlotFunction()
+        # self.PlotFunction()
 
     def LoadData(self):
-        self.data = np.genfromtxt(os.path.join(os.path.dirname(os.path.abspath(__file__)))
-                                  + "/Train.txt",
-                                  delimiter=",")
-        self.data = np.random.rand(self.data.shape[0]).argsort()
+        self.data = np.genfromtxt( "./Train.txt",delimiter=",")
+        print(self.data.shape)
+        # self.data = np.random.rand(self.data.shape[0]).argsort()
         self.label = self.data[:, -1].T
         self.data = self.data[:, :-1].T
 
@@ -53,14 +52,17 @@ class PCA:
         self.projection_list = np.dot(self.egin_vector.T, self.data)
 
     def PlotFunction(self):
-        # self.projection_list = np.dot(self.egin_vector.T, self.data)
-        men = self.projection_list[:, self.label == 0]
-        women = self.projection_list[:, self.label == 1]
+        fake = self.projection_list[:, self.label == 0]
+        genuine = self.projection_list[:, self.label == 1]
+        for i in range(6):
+            plt.figure(figsize=(8, 6))
+            plt.hist(fake[i,:], bins=30, alpha=0.5, label=f'fake')
+            plt.hist(genuine[i,:], bins=30, alpha=0.5, label=f'genuine')
+            plt.title(f'Histogram of PCA Component {i + 1}')
+            plt.xlabel('Projected Feature Value')
+            plt.ylabel('Frequency')
+            plt.legend()
+            plt.savefig("pca_" + str(i+1) + ".png")
 
-        pylab.scatter(men[0, :], men[1, :])
-        pylab.scatter(women[0, :], women[1, :])
-        # pylab.scatter(projection_list[0],projection_list[1])
-        pylab.xlabel('Principal component 1')
-        pylab.ylabel('Principal component 2')
-        pylab.legend(['men', 'women', "allData"])
-        pylab.savefig("pca8.png")
+
+pca = PCA(m=6)
